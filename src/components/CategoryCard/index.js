c731bfa70fcbb4369899b7fbe0c847fa531da558
Augusto Neves <augustoneves@frwk.com.br>
2023-02-01 19:21:55 -0300

    Adjusting CategoryCard component

--- src/components/CategoryCard/index.js
+++ src/components/CategoryCard/index.js
@@ -1,9 +1,15 @@
 import { Text, Image, TouchableOpacity } from "react-native";
+import { urlFor } from "../../services/sanity";
 
 export function CategoryCard({ imgUrl, title }) {
   return (
     <TouchableOpacity activeOpacity={0.7} className="relative mr-2">
-      <Image source={{ uri: imgUrl }} className="h-20 w-20 rounded" />
+      <Image
+        source={{
+          uri: urlFor(imgUrl).auto("format").fit("max").width(500).url(),
+        }}
+        className="h-20 w-20 rounded"
+      />
       <Text className="absolute bottom-1 left-1 text-white font-bold">
         {title}
       </Text>
