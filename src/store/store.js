e8df9be34320eb0d7ca8daa386d6a71cd217abd2
Augusto Neves <augustoneves@frwk.com.br>
2023-02-06 22:47:41 -0300

    Creating restaurant Reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "./reducers/basketSlice";
+import restaurantReducer from "./reducers/restaurantSlice";
+
 
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
+    restaurant: restaurantReducer
   },
 });
