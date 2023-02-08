8b0ba1620062e73d7769a2b6d9adca371240bf9f
Augusto Neves <augustoneves@frwk.com.br>
2023-02-08 17:22:02 -0300

    Adding navigation to Category screen

--- src/components/CategoryCard/index.js
+++ src/components/CategoryCard/index.js
@@ -1,9 +1,16 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text, Image, TouchableOpacity } from "react-native";
 import { urlFor } from "../../services/sanity";
 
-export function CategoryCard({ imgUrl, title }) {
+export function CategoryCard({ imgUrl, title, id }) {
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity activeOpacity={0.7} className="relative mr-2">
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className="relative mr-2"
+      onPress={() => navigation.navigate("category", { id, title })}
+    >
       <Image
         source={{
           uri: urlFor(imgUrl).auto("format").fit("max").width(500).url(),
