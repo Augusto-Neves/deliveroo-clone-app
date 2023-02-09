934dcbf4c59a245c7fc27886ba40e2325bfd7967
Augusto Neves <augustoneves@frwk.com.br>
2023-02-08 23:45:30 -0300

    Adjusting go back button

--- src/components/GoBackButton/index.js
+++ src/components/GoBackButton/index.js
@@ -3,13 +3,13 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import { ArrowLeftIcon } from "react-native-heroicons/outline";
 
-export function GoBackButton() {
+export function GoBackButton({ positions = "top-14 left-4" }) {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      className="absolute top-14 left-4 p-2 bg-gray-100 rounded-full"
+      className={`absolute p-2 bg-gray-100 rounded-full z-50 ${positions}`}
       onPress={() => {
         navigation.goBack();
       }}
